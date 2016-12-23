85728eda6de349584e68a08a972c956f3fe6dca6
Dario Campagna <campagna@esteco.com>
2016-12-23 12:20:39 +0100

    Added tests accessing the file system to assess UnicodeToHtmlTextConverter behavior.

--- test/source/it/esteco/html/ConvertUnicodeTextToHtml.java
+++ test/source/it/esteco/html/ConvertUnicodeTextToHtml.java
@@ -1,5 +1,28 @@
 package it.esteco.html;
 
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class ConvertUnicodeTextToHtml {
 
+    @Test
+    public void convertEmptyUnicodeFile() throws Exception {
+        String path = getClass().getClassLoader().getResource("empty.txt").getPath();
+        UnicodeToHtmlTextConverter textConverter = new UnicodeToHtmlTextConverter(path);
+
+        String html = textConverter.convertToHtml();
+
+        assertEquals("", html);
+    }
+
+    @Test
+    public void convertUnicodeFileText() throws Exception {
+        String path = getClass().getClassLoader().getResource("unicodeText.txt").getPath();
+        UnicodeToHtmlTextConverter textConverter = new UnicodeToHtmlTextConverter(path);
+
+        String html = textConverter.convertToHtml();
+
+        assertEquals("a<br />b &amp; c<br />", html);
+    }
 }
